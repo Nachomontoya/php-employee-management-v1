8bfd6f66474904e6d7eb5040a1957fb44ca86e86
Almego <alvaromerinogar@gmail.com>
2021-07-08 11:33:02 +0200

    finish

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -6,20 +6,27 @@ header("Content-Type: application/json");
 
 switch ($method) {
   case "POST":
-    // var_dump($_POST);
-    $newEmployee = $_POST;
-    echo addEmployee($newEmployee);
+    if($_GET["update"] == true){
+      updateEmployee($_SESSION['employeeUpdate'],$_POST);
+    }else{
+      // var_dump($_POST);
+      $newEmployee = $_POST;
+      echo addEmployee($newEmployee);
+    }
     break;
 
-   case 'GET':
-    //echo var_dump($_GET);
-    $idEmployee = $_GET['ID'];
-    getEmployee($idEmployee);
+  case 'GET':
+    if($_GET["ID"]){
+      $idEmployee = $_GET['ID'];
+      getEmployee($idEmployee);
+    }
     break;
-    
+
   case "DELETE":
     parse_str(file_get_contents("php://input"), $_DELETE);
     $employeeID = $_DELETE['id']; //This is the id of the employee clicked on.
     var_dump(deleteEmployee($employeeID));
+
+
 }
 ?>
